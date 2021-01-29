aed373618242d8b499c9954d5034cb323ec8fb37
Halfinity <ofirhalfin13@gmail.com>
2021-01-29 16:22:43 +0200

    Fix and remove and movments

--- src/main.js
+++ src/main.js
@@ -4,50 +4,57 @@ const sortButton = document.getElementById("sort-button");
 const viewSection = document.getElementById("view-section");
 let taskArr = [];
 
+//if the local storage is empty set empty tasks array else save in the LS
+if (localStorage.getItem("taskArr") === null) {
+  localStorage.setItem("taskArr", "[]");
+} else {
+  let priority = document.getElementById("priority-selector").value;
+  taskArr = JSON.parse(localStorage.getItem("taskArr"));
+  printViewSection(taskArr);
+}
+
 const countText = document.getElementById("counter");
 countText.innerText = `${JSON.parse(localStorage.getItem("taskArr")).length}`;
 
 addButton.addEventListener("click", () => {
-const pin = document.createElement("div");
-pin.className = "pin";
-var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-pin.style.backgroundColor = `#${randomColor}`;
-const task = textInput.value;
-if (task === "" || task === " ") {
-return;
-}
-textInput.value = "";
+  const pin = document.createElement("div");
+  pin.className = "pin";
+  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  pin.style.backgroundColor = `#${randomColor}`;
+  const task = textInput.value;
+  if (task === "" || task === " ") {
+    return;
+  }
+  textInput.value = "";
 
-let priority = document.getElementById("priority-selector").value;
-const todoContainer = document.createElement("div");
-todoContainer.className = "todo-container";
+  let priority = document.getElementById("priority-selector").value;
+  const todoContainer = document.createElement("div");
+  todoContainer.className = "todo-container";
 
-const color = colorTask(priority);
-todoContainer.style.backgroundColor = color;
-
-const priorityDiv = document.createElement("div");
-priorityDiv.innerHTML = priority;
-priorityDiv.className = "todo-priority";
-
-const createdAtDiv = document.createElement("div");
-createdAtDiv.innerHTML = new Date().toDateString();
-createdAtDiv.className = "todo-created-at";
-
-const textDiv = document.createElement("div");
-textDiv.innerHTML = task;
-textDiv.className = "todo-text";
-
-todoContainer.append(pin, priorityDiv, createdAtDiv, textDiv);
-viewSection.appendChild(todoContainer);
-
-taskArr.push({
-priority: priority,
-date: new Date().toDateString(),
-task: task,
-});
+  const priorityDiv = document.createElement("div");
+  priorityDiv.innerHTML = priority;
+  priorityDiv.className = "todo-priority";
 
 
-let newArr = JSON.parse(localStorage.getItem("taskArr"));
+
+  const createdAtDiv = document.createElement("div");
+  createdAtDiv.innerHTML = new Date().toDateString();
+  createdAtDiv.className = "todo-created-at";
+
+  const textDiv = document.createElement("div");
+  textDiv.innerHTML = task;
+  textDiv.className = "todo-text";
+
+  todoContainer.append(pin, priorityDiv, createdAtDiv, textDiv);
+  viewSection.appendChild(todoContainer);
+
+  taskArr.push({
+    priority: priority,
+    date: new Date().toDateString(),
+    task: task,
+  });
+
+  let newArr = JSON.parse(localStorage.getItem("taskArr"));
 
   newArr.push({
     priority: priority,
@@ -86,9 +93,6 @@ function printViewSection(arr) {
     priorityDiv.innerHTML = arr[i].priority;
     priorityDiv.className = "todo-priority";
 
-    const color = colorTask(arr[i].priority);
-    todoContainer.style.backgroundColor = color;
-
     const createdAtDiv = document.createElement("div");
     createdAtDiv.innerHTML = arr[i].date;
     createdAtDiv.className = "todo-created-at";
@@ -100,11 +104,3 @@ function printViewSection(arr) {
     viewSection.appendChild(todoContainer);
   }
 }
-
-if (localStorage.getItem("taskArr") === null) {
-    localStorage.setItem("taskArr", "[]");
-  } else {
-    let priority = document.getElementById("priority-selector").value;
-    taskArr = JSON.parse(localStorage.getItem("taskArr"));
-    printViewSection(taskArr);
-  }
